732d5ce1df685693131913df759b667fc5f1a3ba
Ankit Kumar <myselfankit51@gmail.com>
2021-12-23 01:04:23 +0530

    added nprogress for loading bar

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,40 @@
 import { SessionProvider } from "next-auth/react";
 import { AppProps } from "next/app";
 import { useRouter } from "next/router";
+import nProgress from "nprogress";
+import { useEffect } from "react";
 import Header from "../components/Header";
 import PreviewPlayer from "../components/PreviewPlayer";
 import Sidebar from "../components/Sidebar";
 import { SpotifyProvider } from "../context/SpotifyContext";
 import "../styles/globals.css";
+import "../styles/nprogress.css";
+
+nProgress.configure({
+  showSpinner: false,
+});
 
 function MyApp({ Component, pageProps }: AppProps) {
   const router = useRouter();
+
+  useEffect(() => {
+    const handleStart = (url: string) => {
+      nProgress.start();
+    };
+    const handleStop = () => {
+      nProgress.done();
+    };
+
+    router.events.on("routeChangeStart", handleStart);
+    router.events.on("routeChangeComplete", handleStop);
+    router.events.on("routeChangeError", handleStop);
+
+    return () => {
+      router.events.off("routeChangeStart", handleStart);
+      router.events.off("routeChangeComplete", handleStop);
+      router.events.off("routeChangeError", handleStop);
+    };
+  }, [router]);
 
   return (
     <SessionProvider session={pageProps.session}>
