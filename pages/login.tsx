16e5843c0145277874366507f6e7f5c6ac91500c
ankitk26 <myselfankit51@gmail.com>
2026-01-11 16:04:30 +0530

    show loader

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,30 +1,39 @@
 import Image from "next/image";
 import { signIn } from "next-auth/react";
 import Layout from "../components/layout";
+import { useState } from "react";
+import { RiLoader2Fill } from "react-icons/ri";
 
 export default function Login() {
-  const handleLogin = () => {
-    signIn("spotify", { callbackUrl: "/" });
-  };
+	const [isLoading, setIsLoading] = useState(false);
 
-  return (
-    <Layout title="Log in to Spotify">
-      <div className="flex h-screen w-screen flex-col items-center justify-center gap-20">
-        <Image
-          alt="spotify logo"
-          height={96}
-          // objectFit="contain"
-          src="/images/spotify_logo.png"
-          width={320}
-        />
-        <button
-          className="flex rounded-full bg-primary px-12 py-2 text-lg uppercase tracking-widest hover:bg-primary/80 hover:bg-opacity-80 focus:outline-none"
-          onClick={handleLogin}
-          type="button"
-        >
-          Login
-        </button>
-      </div>
-    </Layout>
-  );
+	const handleLogin = () => {
+		setIsLoading(true);
+		signIn("spotify", { callbackUrl: "/" });
+	};
+
+	return (
+		<Layout title="Log in to Spotify">
+			<div className="flex h-screen w-screen flex-col items-center justify-center gap-20">
+				<Image
+					alt="spotify logo"
+					height={96}
+					// objectFit="contain"
+					src="/images/spotify_logo.png"
+					width={320}
+				/>
+				<button
+					className="relative flex rounded-full bg-primary px-12 py-2 text-lg uppercase tracking-widest hover:bg-primary/80 hover:bg-opacity-80 focus:outline-none h-12 items-center justify-center"
+					onClick={handleLogin}
+					disabled={isLoading}
+					type="button"
+				>
+					<span className={isLoading ? "invisible" : ""}>Login</span>
+					<RiLoader2Fill
+						className={`absolute animate-spin ${isLoading ? "" : "invisible"}`}
+					/>
+				</button>
+			</div>
+		</Layout>
+	);
 }
