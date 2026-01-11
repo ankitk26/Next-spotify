35510eeb77222429a684a507b61e84158467d265
ankitk26 <myselfankit51@gmail.com>
2026-01-11 15:51:43 +0530

    remove logs

--- components/search-input.tsx
+++ components/search-input.tsx
@@ -3,45 +3,44 @@ import { IoCloseOutline, IoSearchOutline } from "react-icons/io5";
 import { useSpotify } from "../context/spotify-context";
 
 export default function SearchInput() {
-  const router = useRouter();
+	const router = useRouter();
 
-  const { query, setQuery } = useSpotify();
+	const { query, setQuery } = useSpotify();
 
-  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    console.log(query);
-    router.push(`/search/${query}`);
-  };
+	const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+		e.preventDefault();
+		router.push(`/search/${query}`);
+	};
 
-  if (!router.pathname.includes("/search")) {
-    return null;
-  }
+	if (!router.pathname.includes("/search")) {
+		return null;
+	}
 
-  return (
-    <form
-      className="flex w-full items-center justify-between gap-3 rounded-full bg-white px-3 py-1.5"
-      onSubmit={handleSubmit}
-    >
-      <IoSearchOutline className="text-2xl text-search-icon" />
+	return (
+		<form
+			className="flex w-full items-center justify-between gap-3 rounded-full bg-white px-3 py-1.5"
+			onSubmit={handleSubmit}
+		>
+			<IoSearchOutline className="text-2xl text-search-icon" />
 
-      <input
-        className="w-full grow bg-transparent font-semibold text-paper text-sm focus:outline-none"
-        onChange={(e) => setQuery(e.target.value)}
-        placeholder="Artists and songs"
-        spellCheck={false}
-        type="text"
-        value={query}
-      />
+			<input
+				className="w-full grow bg-transparent font-semibold text-paper text-sm focus:outline-none"
+				onChange={(e) => setQuery(e.target.value)}
+				placeholder="Artists and songs"
+				spellCheck={false}
+				type="text"
+				value={query}
+			/>
 
-      {query && (
-        <button
-          className="flex items-center focus:outline-none"
-          onClick={() => setQuery("")}
-          type="button"
-        >
-          <IoCloseOutline className="text-2xl text-search-icon" />
-        </button>
-      )}
-    </form>
-  );
+			{query && (
+				<button
+					className="flex items-center focus:outline-none"
+					onClick={() => setQuery("")}
+					type="button"
+				>
+					<IoCloseOutline className="text-2xl text-search-icon" />
+				</button>
+			)}
+		</form>
+	);
 }
